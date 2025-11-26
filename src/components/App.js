73a5f0354d29ac0963e91a63e029895070b97037
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-11-26 22:06:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Review from "./components/Review";
 
 function App() {
-
   return (
     <main>
-    <section className = "container">
-    <h2 id = "review-heading">Our Reviews</h2>
-    </section>
+      <section className="container">
+        <h2 id="review-heading">Our Reviews</h2>
+        <Review />
+      </section>
     </main>
-  )
+  );
 }
+
+export default App;
